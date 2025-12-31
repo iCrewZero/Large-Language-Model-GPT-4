64598505e064573584d764aff66f24fab9155114
Dontamsetty-Tribhuvan <tribhuvan.studyforlife@gmail.com>
2025-12-31 21:58:50 +0530

    Implement RMSNorm layer in rmsnorm.py

---
+++ model/rmsnorm.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim, eps=1e-6):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(dim))
+        self.eps = eps
+
+    def forward(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight
