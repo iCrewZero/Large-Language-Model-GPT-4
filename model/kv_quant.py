0776511dc5853232e885ed79db6ec660c133c2e2
Dontamsetty-Tribhuvan <tribhuvan.studyforlife@gmail.com>
2026-01-14 19:24:25 +0530

    Update kv_quant.py

--- model/kv_quant.py
+++ model/kv_quant.py
@@ -1,10 +1,10 @@
 import torch
 
-def quantize_kv(x):
-    max_val = x.abs().amax(dim=(-1, -2), keepdim=True)  # per head
+def quantize_k(x):
+    max_val = x.abs().amax(dim=(-1, -2), keepdim=True)
     scale = max_val / 127.0 + 1e-6
-    x_int8 = torch.clamp((x / scale).round(), -128, 127).to(torch.int8)
-    return x_int8, scale
+    x_i8 = torch.clamp((x / scale).round(), -128, 127).to(torch.int8)
+    return x_i8, scale
 
-def dequantize_kv(x_int8, scale):
-    return x_int8.float() * scale
+def dequantize_k(x_i8, scale):
+    return x_i8.float() * scale
