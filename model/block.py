5d4936875a2c1a97dbde7af25faeedf0d24d8ca6
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-16 10:40:11 +0530

    Update block.py

--- model/block.py
+++ model/block.py
@@ -1,34 +1,16 @@
-import torch
 import torch.nn as nn
-from .attention import Attention
+from model.attention import Attention
+from model.moe import MoE
 
 class Block(nn.Module):
-    def __init__(self, dim, heads):
+    def __init__(self, cfg):
         super().__init__()
-        self.ln1 = nn.LayerNorm(dim)
-        self.ln2 = nn.LayerNorm(dim)
+        self.ln1 = nn.LayerNorm(cfg.dim)
+        self.attn = Attention(cfg)
+        self.ln2 = nn.LayerNorm(cfg.dim)
+        self.moe = MoE(cfg.dim, cfg.moe_experts, cfg.moe_top_k)
 
-        self.qkv = nn.Linear(dim, dim * 3)
-        self.attn = Attention(dim, heads)
-
-        self.ff = nn.Sequential(
-            nn.Linear(dim, 4 * dim),
-            nn.GELU(),
-            nn.Linear(4 * dim, dim)
-        )
-
-    def forward(self, x):
-        B, T, C = x.shape
-        qkv = self.qkv(self.ln1(x))
-        q, k, v = qkv.chunk(3, dim=-1)
-
-        q = q.view(B, T, -1, C // 8).transpose(1, 2)
-        k = k.view(B, T, -1, C // 8).transpose(1, 2)
-        v = v.view(B, T, -1, C // 8).transpose(1, 2)
-
-        out = self.attn(q, k, v)
-        out = out.transpose(1, 2).contiguous().view(B, T, C)
-
-        x = x + out
-        x = x + self.ff(self.ln2(x))
+    def forward(self, x, *kv):
+        x = x + self.attn(self.ln1(x), *kv)
+        x = x + self.moe(self.ln2(x))
         return x
