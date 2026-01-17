70955eb2d776159c23473e2ad068d8e655b674a9
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-17 21:21:13 +0530

    Update optimizer.py

--- training/optimizer.py
+++ training/optimizer.py
@@ -1,21 +1,9 @@
 import torch
 
-def build_optimizer(model, lr, weight_decay):
-    decay, no_decay = [], []
-
-    for name, param in model.named_parameters():
-        if not param.requires_grad:
-            continue
-        if param.ndim >= 2 and "norm" not in name:
-            decay.append(param)
-        else:
-            no_decay.append(param)
-
+def build_optimizer(model, lr=3e-4, wd=0.1):
     return torch.optim.AdamW(
-        [
-            {"params": decay, "weight_decay": weight_decay},
-            {"params": no_decay, "weight_decay": 0.0},
-        ],
+        model.parameters(),
         lr=lr,
+        weight_decay=wd,
         betas=(0.9, 0.95)
     )
