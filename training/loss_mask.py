d6fea057709433e866e4e955820e7f7a5aba3470
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-17 21:22:58 +0530

    Update loss_mask.py

--- training/loss_mask.py
+++ training/loss_mask.py
@@ -1,17 +1,4 @@
-IGNORE_TOKENS = {
-    "<|system|>",
-    "<|user|>",
-    "<|think|>",
-    "<|reflect|>"
-}
+import torch
 
-def build_loss_mask(token_ids, tokenizer):
-    mask = []
-    ignore_ids = {tokenizer.encode(t)[0] for t in IGNORE_TOKENS}
-
-    for t in token_ids:
-        if t in ignore_ids:
-            mask.append(-1)
-        else:
-            mask.append(t)
-    return mask
+def causal_mask(seq_len):
+    return torch.tril(torch.ones(seq_len, seq_len)).bool()
