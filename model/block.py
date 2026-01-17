7605ac03daf691958e82f4f6db874f2a56c03a1e
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-17 10:21:43 +0530

    Update block.py

--- model/block.py
+++ model/block.py
@@ -1,16 +1,38 @@
 import torch.nn as nn
-from model.attention import Attention
-from model.moe import MoE
+from .rmsnorm import RMSNorm
+from .attention import CausalSelfAttention
+from .mla import MLA
+from .moe import MoE
 
 class Block(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self, cfg, rope):
         super().__init__()
-        self.ln1 = nn.LayerNorm(cfg.dim)
-        self.attn = Attention(cfg)
-        self.ln2 = nn.LayerNorm(cfg.dim)
-        self.moe = MoE(cfg.dim, cfg.moe_experts, cfg.moe_top_k)
+        self.norm1 = RMSNorm(cfg.dim)
+        self.norm2 = RMSNorm(cfg.dim)
 
-    def forward(self, x, *kv):
-        x = x + self.attn(self.ln1(x), *kv)
-        x = x + self.moe(self.ln2(x))
+        self.attn = CausalSelfAttention(
+            cfg.dim, cfg.n_head, cfg.n_kv_head, rope
+        )
+
+        self.mla = MLA(cfg.dim, cfg.n_head) if cfg.use_mla else None
+        self.moe = MoE(cfg.dim, cfg.moe_experts, cfg.moe_topk) if cfg.use_moe else None
+
+        self.ffn = nn.Sequential(
+            nn.Linear(cfg.dim, 4*cfg.dim),
+            nn.GELU(),
+            nn.Linear(4*cfg.dim, cfg.dim)
+        )
+
+    def forward(self, x, pos):
+        h = self.attn(self.norm1(x), pos)
+        if self.mla:
+            h = h + self.mla(h)
+
+        x = x + h
+
+        if self.moe:
+            x = x + self.moe(self.norm2(x))
+        else:
+            x = x + self.ffn(self.norm2(x))
+
         return x
