e0cbdbc199e061a45c62b6c38a53f1f39be4b890
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-17 21:23:48 +0530

    Update mtp_loss.py

--- training/mtp_loss.py
+++ training/mtp_loss.py
@@ -1,11 +1,13 @@
 import torch
 import torch.nn.functional as F
 
-def mtp_loss(logits, targets, k=3):
-    loss = 0
-    for i in range(1, k + 1):
+def mtp_loss(hidden, labels, k=3):
+    loss = 0.0
+    for i in range(1, k+1):
+        pred = hidden[:, :-i]
+        target = labels[:, i:]
         loss += F.cross_entropy(
-            logits[:, :-i].reshape(-1, logits.size(-1)),
-            targets[:, i:].reshape(-1)
+            pred.reshape(-1, pred.size(-1)),
+            target.reshape(-1)
         )
-    return loss / k
+    return loss * 0.1
