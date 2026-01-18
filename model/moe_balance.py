eaddecb010869176ccf02c748d3c1482fbd81159
iCrewZero <tribhuvan.studyforlife@gmail.com>
2026-01-18 10:11:20 +0530

    Update moe_balance.py

--- model/moe_balance.py
+++ model/moe_balance.py
@@ -1,3 +1,6 @@
-def balance_penalty(router_probs):
-    usage = router_probs.mean(dim=(0,1))
-    return usage.var()
+import torch
+
+def load_balance_loss(router_probs):
+    density = router_probs.mean(dim=(0,1))
+    loss = density.pow(2).mean()
+    return loss
